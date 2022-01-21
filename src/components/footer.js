c288887186a93be093f31b358c11fe53efe9e86d
seanmorales <seanmorales44@gamil.com>
2022-01-20 20:33:22 -0600

    updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 
 import Header from ".header"
-import Footer from ".footer"
-
-import dummyData from "./dummyData"
-
+import Forter from "./footer"
 
 export default class App extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      monthData: dummyData.data[0]
-    }
-  }
   render() {
     return (
       <div className='page-wrapper'>
         <Header month="January" />
-        <Footer year="2022" />
       </div>
     );
   }
--- src/header.js
+++ src/header.js
@@ -9,3 +9,4 @@ export default function header(props) {
     </div>
   )
 }
+
